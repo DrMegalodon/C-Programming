8bd73c4a3963fac27cad9e0a8f7db1d6e503e0c3
DrMegalodon <62468987+DrMegalodon@users.noreply.github.com>
2020-03-23 13:16:11 +0300

    2

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 #include <cmath>
 
-void funct(int &ptrA) {
-    ptrA = pow(ptrA,2);
-}
-
-int main1()
-{
-    int a = 11;
-    funct(a);
-    std::cout << a << std::endl;
+void funct(int& ptrA) {
+    ptrA = pow(ptrA, 2);
 }
 
 // нахождение n-ого числв фибоначчи интеративным способом
@@ -27,19 +20,14 @@ int func(int c) {
     return c;
 }
 
-int main2()
-{   n = 10;
-    std::cout << func(n-1) << std::endl;
-} 
-
 // нахождение n-ого числв фибоначчи рекурсивным способом
 
 int fu(int c) {
     if (c == 0) {
-        return 0; 
+        return 0;
     }
     if (c == 1 || c == 2) {
-        return 1; 
+        return 1;
     }
     else {
         c = fu(c - 2) + fu(c - 1);
@@ -47,8 +35,19 @@ int fu(int c) {
     }
 }
 
-int main3()
+int main()
 {
+    // вывод для первого задания
+    int a = 11;
+    funct(a);
+    std::cout << a << std::endl;
+
+    // вывод для второго задания
     int n = 10;
-    std::cout << fu(n-1) << std::endl;
+
+    // интеративным методом
+    std::cout << func(n - 1) << std::endl;
+
+    // рекурсивным методом
+    std::cout << fu(n - 1) << std::endl;
 }
