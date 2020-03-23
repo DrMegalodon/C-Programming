611bda9cd3550091c29b6b688003fedddaf0f8d3
DrMegalodon <62468987+DrMegalodon@users.noreply.github.com>
2020-03-23 13:08:13 +0300

    2

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
+#include <cmath>
 
-double fu(int g) {
-    g = g * g;
-    return g;
+void funct(int &ptrA) {
+    ptrA = pow(ptrA,2);
 }
 
-int main()
+int main1()
 {
     int a = 11;
-    a = fu(a);
-    std::cout << fu(a) << "\t" << a << std::endl;
+    funct(a);
+    std::cout << a << std::endl;
+}
+
+// нахождение n-ого числв фибоначчи интеративным способом
+
+int func(int c) {
+    int f = 0;
+    int f2 = 1;
+    int t = 0;
+    for (int i = 0; i < c; i++) {
+        t = f2;
+        f2 = f + f2;
+        f = t;
+    }
+    c = f;
+    return c;
+}
+
+int main2()
+{   n = 10;
+    std::cout << func(n-1) << std::endl;
+} 
+
+// нахождение n-ого числв фибоначчи рекурсивным способом
+
+int fu(int c) {
+    if (c == 0) {
+        return 0; 
+    }
+    if (c == 1 || c == 2) {
+        return 1; 
+    }
+    else {
+        c = fu(c - 2) + fu(c - 1);
+        return c;
+    }
+}
+
+int main3()
+{
+    int n = 10;
+    std::cout << fu(n-1) << std::endl;
 }
